0b2ff1d4d6cb82a289c37f4d1fa76a5c4ab42e1a
iwansafr <iwansafr@gmail.com>
2022-04-02 20:44:00 +0700

    head css

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -11,22 +11,7 @@
 
   <!-- Favicons -->
   <link href="/public/templates/esoftgreat/images/favicon.ico" rel="icon">
-  <link href="/public/templates/esoftgreat/images/small-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="/public/templates/esoftgreat/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="/public/templates/esoftgreat/assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="/public/templates/esoftgreat/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="/public/templates/esoftgreat/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="/public/templates/esoftgreat/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="/public/templates/esoftgreat/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="/public/templates/esoftgreat/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="/public/templates/esoftgreat/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-  <link href="/public/templates/esoftgreat/assets/css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="/public/templates/esoftgreat/head.css">
 </head>
 
 <body>
